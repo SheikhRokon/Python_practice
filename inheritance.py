3b362541c0ce6de50e45a447008c3666d79cb9d5
Sheikh Rokon Uzzman <mdorokon.u545@gmail.com>
2023-09-03 17:24:12 +0600

    pultilevel Inheritance

--- inheritance.py
+++ inheritance.py
@@ -1,18 +1,40 @@
 # multiple inheritance
+# class baba:
+#     car = 'BMW'
+#     home = "10 floor"
+#     tk = "100 M"
+    
+# class kaka1:
+#     bike = 'R1 5'
+#     Mobile = 'iphone'
+    
+# class kaka2:
+#     bike = 'suzuki'
+#     Mobile = 'Redmi'
+# class kaka3(baba,kaka1):
+#     bike = 'suzuki'
+#     Mobile = 'Redmi'
+    
+# print(kaka3.tk)
+
+# pultilevel Inheritance
+
 class baba:
     car = 'BMW'
     home = "10 floor"
     tk = "100 M"
     
-class kaka1:
+class son1(baba):
     bike = 'R1 5'
     Mobile = 'iphone'
     
-class kaka2:
+class son2(son1):
     bike = 'suzuki'
     Mobile = 'Redmi'
-class kaka3(baba,kaka1):
-    bike = 'suzuki'
-    Mobile = 'Redmi'
+class son3(son2):
+    bike = 'TVS'
+    Mobile = 'Oppo'
     
-print(kaka3.tk)        
+print(son3.car)    
+
+       
