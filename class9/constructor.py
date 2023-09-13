8037e4bfa126bd3e297ef59e8b3d9d4b99b5fec4
Sheikh Rokon Uzzman <mdorokon.u545@gmail.com>
2023-09-13 16:55:40 +0600

    constructor

---
+++ class9/constructor.py
@@ -0,0 +1,19 @@
+class Profile:
+    def sumon( salf, name,age):
+        print(f"my name is {name} and my age is {age}")
+    def khalid( salf, name,age):
+        print(f" {name} and my age is {age}")
+
+
+x = Profile()
+
+x.sumon("rokon",10) 
+x.khalid("rokon",10) 
+
+
+# class MyProfile:
+#     def __init__(self,name,age):
+#         print(f"my name is {name} and my age is {age}")
+        
+# x = MyProfile("rkon",10)               
+        
